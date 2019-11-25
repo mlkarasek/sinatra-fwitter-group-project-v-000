20c382899ad7bb60005e973f8449426f1de9f918
Monica  Karasek <mlkarasek@gmail.com>
2019-11-25 21:11:04 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,53 +1,78 @@
-class UsersController < ApplicationController
+class TweetsController < ApplicationController
 
-  get '/signup' do
+  get '/tweets' do
     if logged_in?
-      redirect "/tweets"
+      @tweets = Tweet.all
+      erb :'/tweets/index'
     else
-      erb :'/users/signup'
-    end
-  end
-
-  post '/signup' do
-    user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    if user.save
-      session[:user_id] = user.id
-      redirect "/tweets"
-    else
-      flash[:message] = "Please enter a valid username, email, and password to sign up."
-      redirect "/signup"
-    end
-  end
-
-  get '/login' do
-    if logged_in?
-      redirect "/tweets"
-    else
-      erb :'users/login'
-    end
-  end
-
-  post '/login' do
-    user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect "/tweets"
-    else
-      flash[:message] = "Incorrect username, email, or password entered."
       redirect "/login"
     end
   end
 
-  get '/logout' do
+  get '/tweets/new' do
     if logged_in?
-      session.clear
+      erb :'/tweets/new'
+    else
+      redirect "/login"
     end
-    redirect "/login"
   end
 
-  get '/users/:slug' do
-    @tweets = Tweet.all
-    erb :'/users/show'
+  post '/tweets' do
+    if logged_in? && params[:tweet][:content] != ""
+      @tweet = Tweet.create(params[:tweet])
+      @tweet.user = current_user
+      @tweet.save
+      flash[:message] = "Tweet successfully created."
+      redirect "/tweets/#{@tweet.id}"
+    elsif logged_in? && params[:tweet][:content] == ""
+      flash[:message] = "Please enter content to post a tweet."
+      redirect "/tweets/new"
+    else
+      redirect "/login"
+    end
+  end
+
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'/tweets/show'
+    else
+      redirect "/login"
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if logged_in? && @tweet.user == current_user
+      erb :'/tweets/edit'
+    elsif logged_in? && @tweet.user != current_user
+      redirect "/tweets"
+    else
+      redirect "/login"
+    end
+  end
+
+  patch '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if params[:tweet][:content] != ""
+      @tweet.update(params[:tweet])
+      flash[:message] = "Tweet successfully updated."
+      redirect "/tweets/#{@tweet.id}"
+    elsif params[:tweet][:content] == ""
+      flash[:message] = "Please enter content to update tweet."
+      redirect "/tweets/#{@tweet.id}/edit"
+    end
+  end
+
+  delete '/tweets/:id/delete' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if logged_in? && @tweet.user == current_user
+      @tweet.delete
+      flash[:message] = "Tweet successfully deleted."
+      redirect "/tweets"
+    else
+      redirect "/login"
+    end
   end
 
 end
