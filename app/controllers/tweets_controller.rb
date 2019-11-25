b3529ef4abb4e52372bd930a706b55f94bbf89ce
Monica  Karasek <mlkarasek@gmail.com>
2019-11-25 21:01:48 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,53 @@
-class TweetsController < ApplicationController
+class UsersController < ApplicationController
 
+  get '/signup' do
+    if logged_in?
+      redirect "/tweets"
+    else
+      erb :'/users/signup'
+    end
+  end
+
+  post '/signup' do
+    user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    if user.save
+      session[:user_id] = user.id
+      redirect "/tweets"
+    else
+      flash[:message] = "Please enter a valid username, email, and password to sign up."
+      redirect "/signup"
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect "/tweets"
+    else
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/tweets"
+    else
+      flash[:message] = "Incorrect username, email, or password entered."
+      redirect "/login"
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+    end
+    redirect "/login"
+  end
+
+  get '/users/:slug' do
+    @tweets = Tweet.all
+    erb :'/users/show'
+  end
 
 end
