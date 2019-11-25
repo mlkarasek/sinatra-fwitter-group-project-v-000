b2a09430e735a838769674a4ef0d1458af236972
Monica  Karasek <mlkarasek@gmail.com>
2019-11-25 21:09:54 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190117194107) do
+ActiveRecord::Schema.define(version: 20191122223617) do
 
   create_table "tweets", force: :cascade do |t|
-    t.string  "content"
+    t.text    "content"
     t.integer "user_id"
   end
 
